532fc2d02f5c7db356c5152625e5d53fb94eb66d
x4trm <damian.maza@wp.pl>
2022-10-10 00:00:41 +0200

    add files

---
+++ metody/metody/Factorial.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metody
+{
+    public class Factorial
+    {
+        public int n { get; set; }
+        public Factorial(int n)
+        {
+            if (n < 0)
+            {
+                throw new Exception("n<0");
+            }
+            else
+            {
+                this.n = n;
+            }
+        }
+        public int SolveInt()
+        {
+            int result = 1;
+            if(n == 0)
+            {
+                return result;
+            }
+            while(n> 0)
+            {
+                result*=n;
+                n--;
+            }
+            return result;
+        }
+        public short SolveShort()
+        {
+            short result = 1;
+            if (n == 0)
+            {
+                return result;
+            }
+            while (n > 0)
+            {
+                result *= Convert.ToInt16(n);
+                n--;
+            }
+            return result;
+        }
+        public long SolveLong()
+        {
+            long result = 1;
+            if (n == 0)
+            {
+                return result;
+            }
+            while (n > 0)
+            {
+                result *= Convert.ToInt64(n);
+                n--;
+            }
+            return result;
+        }
+    }
+}
