c089cb210bebbf63e4dd8761186996441f7a81eb
x4trm <damian.maza@wp.pl>
2022-11-24 22:21:49 +0100

    add files

--- metody/metody/Matrix.cs
+++ metody/metody/Matrix.cs
@@ -4,7 +4,7 @@ namespace metody;
 
 public static class Matrix
 {
-    public static double[,] mnozenie(double[,] a, double[,] b)
+    public static double[,] Mnozenie(double[,] a, double[,] b)
     {
         int liczbaWierszyA = a.GetLength(0);
         int liczbaKolumnA = a.GetLength(1);
@@ -30,5 +30,79 @@ public static class Matrix
         {
             throw new Exception("Error");
         }
+    }
+    public static double[,] MnozeniePrzezSkalar(double[,] a,double b)
+    {
+        int liczbaWierszyA = a.GetLength(0);
+        int liczbaKolumnA = a.GetLength(1);
+        for(int i=0;i<liczbaWierszyA;i++)
+        {
+            for(int j=0;j<liczbaKolumnA;j++)
+            {
+                a[i,j]*=b;
+            }
+        }
+        return a;
+    }
+    public static double Wyznacznik2x2(double[,] a)
+    {
+        return a[0,0]*a[1,1]-a[0,1]*a[1,0];
+    }
+    public static double Wyznacznik3x3(double[,] a)
+    {
+        double A=a[0,0]*a[1,1]*a[2,2]+a[1,0]*a[2,1]*a[0,2]+a[2,0]*a[0,1]*a[1,2];
+        double B=a[1,0]*a[0,1]*a[2,2]+a[0,0]*a[2,1]*a[1,2]+a[2,0]*a[1,1]*a[0,2];
+        return A-B;
+    }
+    public static double[,] Transpose(double[,] a)
+    {
+        int liczbaWierszyA = a.GetLength(0);
+        int liczbaKolumnA = a.GetLength(1);
+        double[,] result=new double[liczbaKolumnA,liczbaWierszyA];
+        for(int i=0;i<liczbaWierszyA;i++)
+        {
+            for(int j=0;j<liczbaKolumnA;j++)
+            {
+                result[j,i]=a[i,j];
+            }
+        }
+        return result;
+    }
+    public static double[] Gauss(double[,] a,double[]b)
+    {
+        int n = a.GetLength(0);
+        double[] result=new double[n];
+        double tmp=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {    
+                if(j>i)
+                {
+                    tmp = a[j,i] / a[i,i]; // mnożnik
+                    for(int k=i;k<n+1;k++)
+                    {
+                        if(k!=n)
+                        {
+                            a[j,k]-=tmp*a[i,k];
+                        }
+                        else{
+                            b[j]=b[j]-tmp*b[i];
+                        }
+                    }// petla po kolumnach
+                }
+            }
+        }
+
+        for(int i=n-1;i>=0;i--)
+        {
+            tmp=0;
+            for(int j=i;j<=n-1;j++)
+            {
+                tmp+=a[i,j]*result[j];
+            }
+            result[i]=(b[i]-tmp)/a[i,i];
+        }
+        return result;
     }
 }
--- metody/metody/Program.cs
+++ metody/metody/Program.cs
@@ -3,23 +3,30 @@ namespace metody
 {
     public class Program
     {
-        static void MnozenieDwochMacierzy()
+        static void foo()
         {
             double[,] A = { { -1, 4, 2, -2 }, { 1, 2, -3, 0 }, { -1, 0, 0, 5 } };
             double[,] B = { { 2, -1 }, { 1, 3 }, { -2, 0 }, { 0, -4 } };
-            double[,] result = Matrix.mnozenie(A, B);
-            for (int i = 0; i < result.GetLength(0); i++)
+            double[,] C = {{-1,2,-3,3,5},{8,0,7,4,-5},{-3,4,-3,2,-2},{8,-3,-2,1,2},{-2,-1,-6,9,0}};
+            double[] D={56,62,-10,14,28};
+            double []result=Matrix.Gauss(C,D);
+            for(int i=0;i<result.GetLength(0);i++)
             {
-                for (int j = 0; j < result.GetLength(1); j++)
-                {
-                    Console.Write(result[i, j] + " ");
-                }
-                Console.WriteLine();
+                Console.WriteLine(result[i]);
             }
+            // double[,] result = Matrix.Transpose(B);
+            // for (int i = 0; i < result.GetLength(0); i++)
+            // {
+            //     for (int j = 0; j < result.GetLength(1); j++)
+            //     {
+            //         Console.Write(result[i, j] + " ");
+            //     }
+            //     Console.WriteLine();
+            // }
         }
         static void Main(string[] args)
         {
-            MnozenieDwochMacierzy();
+            foo();
         }
     }
 };
