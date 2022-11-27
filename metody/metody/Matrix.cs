68e32dee1b1b291d76e1e918c3d8c34a1852a46a
x4trm <damian.maza@wp.pl>
2022-11-27 23:15:09 +0100

    git add laplace&sarrus

--- metody/metody/Matrix.cs
+++ metody/metody/Matrix.cs
@@ -68,6 +68,76 @@ public static class Matrix
         }
         return result;
     }
+    private static double[,] DeleteRowColumn(double [,] a,int row,int column)
+    {
+        int r=a.GetLength(0)-1;
+        int c=a.GetLength(1)-1;
+        double[,] result=new double[r,c];
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                int tmpR=i<row?i:i+1;
+                int tmpC=j<column?j:j+1;
+                result[i,j]=a[tmpR,tmpC];
+            }
+        }
+        return result;
+    }
+    public static double LaplaceDet(double[,]a)
+    {
+        int liczbaWierszy=a.GetLength(0);
+        int liczbaKolumn=a.GetLength(1);
+        double _sum=0;
+        if(liczbaWierszy==liczbaKolumn)
+        {
+            if(liczbaWierszy==1)
+            {
+                return a[0,0];
+            }
+            for(int i=0;i<liczbaKolumn;i++)
+            {
+                double tmp=a[0,i];
+                double b=LaplaceDet(DeleteRowColumn(a,0,i));
+                if(i%2==0)
+                {
+                    _sum+=tmp*b;
+                }
+                else{
+                    _sum-=tmp*b;
+                }
+            }
+            return _sum;
+        }
+        else{
+            throw new Exception("Error");
+        }
+    }
+    public static double[,] MacierzOdwrotna(double[,] a)
+    {
+        int liczbaWierszy=a.GetLength(0);
+        int liczbaKolumn=a.GetLength(1);
+        if(liczbaKolumn==liczbaWierszy)
+        {
+            double wyznacznik=LaplaceDet(a);
+            double[,] result=new double[liczbaWierszy,liczbaKolumn];
+            for(int i=0;i<liczbaWierszy;i++)
+            {
+                for(int j=0;j<liczbaKolumn;j++)
+                {
+                    result[i,j]=1.0/wyznacznik*LaplaceDet(DeleteRowColumn(a,i,j));
+                    if((i+j)%2==1)
+                    {
+                        result[i,j]=-result[i,j];
+                    }
+                }
+            }
+            return Transpose(result);
+        }
+        else{
+            throw new Exception("Error");
+        }
+    }
     public static double[] Gauss(double[,] a,double[]b)
     {
         int n = a.GetLength(0);
@@ -80,29 +150,45 @@ public static class Matrix
                 if(j>i)
                 {
                     tmp = a[j,i] / a[i,i]; // mnożnik
-                    for(int k=i;k<n+1;k++)
+                    for(int k=i+1;k<n+1;k++)
                     {
                         if(k!=n)
                         {
                             a[j,k]-=tmp*a[i,k];
                         }
                         else{
-                            b[j]=b[j]-tmp*b[i];
+                            b[j]-=tmp*b[i];
                         }
                     }// petla po kolumnach
                 }
             }
         }
-
-        for(int i=n-1;i>=0;i--)
-        {
-            tmp=0;
-            for(int j=i;j<=n-1;j++)
-            {
-                tmp+=a[i,j]*result[j];
-            }
-            result[i]=(b[i]-tmp)/a[i,i];
-        }
+        // for(int i=n;n>=0;i--)
+        // {
+        //     result[i]=b[i]/a[i,i];
+        //     for(int j=i;j>=0;j--)
+        //     {
+        //         a[i,i]+=a[j,i]*result[i];
+        //     }
+        // }
+        // for(int i=n-1;i>=0;i--)
+        // {
+        //     result=
+        //     for(int j=i;j<=n-1;j++)
+        //     {
+        //         tmp+=a[i,j]*result[j];
+        //     }
+        //     result[i]=(b[i]-tmp)/a[i,i];
+        // }
+        // for(int i=n-1;i>=0;i--)
+        // {
+        //     tmp=0;
+        //     for(int j=i;j<=n-1;j++)
+        //     {
+        //         tmp+=a[i,j]*result[j];
+        //     }
+        //     result[i]=(b[i]-tmp)/a[i,i];
+        // }
         return result;
     }
 }
--- metody/metody/Program.cs
+++ metody/metody/Program.cs
@@ -9,20 +9,22 @@ namespace metody
             double[,] B = { { 2, -1 }, { 1, 3 }, { -2, 0 }, { 0, -4 } };
             double[,] C = {{-1,2,-3,3,5},{8,0,7,4,-5},{-3,4,-3,2,-2},{8,-3,-2,1,2},{-2,-1,-6,9,0}};
             double[] D={56,62,-10,14,28};
-            double []result=Matrix.Gauss(C,D);
-            for(int i=0;i<result.GetLength(0);i++)
-            {
-                Console.WriteLine(result[i]);
-            }
-            // double[,] result = Matrix.Transpose(B);
-            // for (int i = 0; i < result.GetLength(0); i++)
+            double[,] E={{1,3,2},{4,-1,2},{1,-1,0}};
+            double [,]result=Matrix.MacierzOdwrotna(E);
+            // Console.Write(Matrix.LaplaceDet(E));
+            // for(int i=0;i<result.GetLength(0);i++)
             // {
-            //     for (int j = 0; j < result.GetLength(1); j++)
-            //     {
-            //         Console.Write(result[i, j] + " ");
-            //     }
-            //     Console.WriteLine();
+            //     Console.WriteLine(result[i]);
             // }
+            // double[,] result = Matrix.Transpose(B);
+            for (int i = 0; i < result.GetLength(0); i++)
+            {
+                for (int j = 0; j < result.GetLength(1); j++)
+                {
+                    Console.Write(result[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
         static void Main(string[] args)
         {
